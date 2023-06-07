fbf6e968e31d40a60ac86403c86124b83ed53e1f
geon076 <129704478+geon076@users.noreply.github.com>
2023-06-07 16:15:41 +0900

    qwerty inner

---
+++ script.js
@@ -0,0 +1,13 @@
+/*let div = document.getElementById("box");
+
+div.innerHTML = "hello world";
+
+div.style.color = "magenta";*/
+
+let clicker = document.getElementById("clicker");
+
+clicker.addEventListener("click", () => {
+    console.log("123");
+});
+
+div.innerHTML = "times : ++";
