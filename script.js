cf491b71a2199eb5d9f4eb82fa168ebbe8f9882c
geon076 <129704478+geon076@users.noreply.github.com>
2023-06-14 16:10:23 +0900

    send

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-/*let div = document.getElementById("box");
-
-div.innerHTML = "hello world";
-
-div.style.color = "magenta";*/
-
-let clicker = document.getElementById("clicker");
-
-clicker.addEventListener("click", () => {
-    console.log("123");
-});
-
-div.innerHTML = "times : ++";
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`server is running ${PORT}`) ;
+})
+app.post('/', (req, res) => {
+    let query = req.query;
+    res.send(`<h1>hello ${query.name}!</h1>`);
+})
